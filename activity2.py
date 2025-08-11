9adcff6a74482e66bf68ef0e8f94572330a0f34d
unknown <mjperez0931@gmail.com>
2025-08-11 22:37:19 +0800

    reversed it back to the old code

--- activity2.py
+++ activity2.py
@@ -1,8 +1,3 @@
 name = input("Whats your name?: ")
-#  it actually works yay
-hallo = input("hello " + name + " welcome my python code do you like it? (yes or no): ")
-# i made some improvement 
-if hallo.lower() == 'yes': 
-    print("Thanks") 
-if hallo.lower() == 'no':
-    print("oh ok T-T")
+
+print("hello",name,"welcome my python code")
