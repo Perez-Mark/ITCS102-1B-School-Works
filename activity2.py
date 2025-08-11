c2b449532002769f05dfcbeb0d5267662c20e532
unknown <mjperez0931@gmail.com>
2025-08-11 22:27:26 +0800

    code_challenge

--- activity2.py
+++ activity2.py
@@ -1,6 +1,6 @@
 name = input("Whats your name?: ")
 #  it actually works yay
-hallo = input("hello " + name + " welcome my python code do you like it?: ")
+hallo = input("hello " + name + " welcome my python code do you like it? (yes or no): ")
 # i made some improvement 
 if hallo.lower() == 'yes': 
     print("Thanks") 
