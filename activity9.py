6fd3b9677f50f9a4817955676c7a029fe48961fb
unknown <mjperez0931@gmail.com>
2025-08-20 14:51:48 +0800

    activity9

---
+++ activity9.py
@@ -0,0 +1,12 @@
+a = 5
+b = 7
+
+c = 3
+d = 2
+username = 'Perez'
+password = 'mj1234'
+print(( username== 'Perez' and password == 'mj1234' ))
+
+print ( (a > b) and (c < d) )
+print( (a > b) or (c < d ) )
+print(not(a > b) or (C < d) )
