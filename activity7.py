7613f72c3393eb81efccdb582b72649ed2ce99dc
unknown <mjperez0931@gmail.com>
2025-08-20 14:41:29 +0800

    activity7

---
+++ activity7.py
@@ -0,0 +1,20 @@
+a = 5
+
+print("The value of a is", a)
+
+a += 5
+a -= 6
+a *= 4
+a += 3
+
+print("The value of a is", a)
+
+b = 3
+
+print("The value of b is", b)
+
+b += 3
+b *= 3
+b -= 3
+
+print("The value of b is", b)
