f9ac9c85cfc760a0c2166acd4457b07bfa07728e
Perez. Mark Joseph A. <89617439+Perez-Mark@users.noreply.github.com>
2025-09-12 16:35:59 -0700

    activity10

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "Mj"
+password = "mj123"
+
+usern = input("Username: ")
+passw = input("Password: ")
+
+if usern == "Mj" and passw == "mj123":
+    print("Welcome",usern)
+else:
+    print("Wrong password or username")
